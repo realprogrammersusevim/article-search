45842757fc7c540b959433a640dc61dd1aab7090
Jonathan <git@jonathansm.dev>
2024-11-22 17:36:33 -0600

    refactor(utils.py): remove unused functions and add metadata to Article

--- utils.py
+++ utils.py
@@ -1,22 +1,4 @@
-import os
-from typing import List
-from openai import OpenAI
-import markdown
-from flask_socketio import emit
-
-import tantivy
-
-
-# Create the tantivy index
-def create_index():
-    schema_builder = tantivy.SchemaBuilder()
-    schema_builder.add_text_field("title", stored=True)
-    schema_builder.add_text_field("body", stored=True)
-    schema_builder.add_text_field("id", stored=True, tokenizer_name="raw")
-    schema = schema_builder.build()
-
-    index = tantivy.Index(schema, path=os.getcwd() + "/index")
-    return index
+import datetime
 
 
 class Article:
@@ -25,10 +7,10 @@ class Article:
         self.body = body
         self.id = id
         self.embedding = None
-        self.similarity = None
         self.date = None
         self.url = None
         self.publication = None
+        self.score = None
 
     def serializable(self):
         to_serial = self
@@ -37,35 +19,9 @@ class Article:
         to_serial.similarity = None
         return to_serial.__dict__
 
-
-def summarize_article(article: Article):
-    client = OpenAI(api_key="lol", base_url="http://localhost:11434/v1")
-    prefill = (
-        "#### Main Thesis\n\n"  # Prefill the LLM to get it started on the right track
-    )
-    stream = client.chat.completions.create(
-        model="llama3",
-        stream=True,
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a seasoned writer and summarizer.",
-            },
-            {
-                "role": "user",
-                "content": f"Use the following format to provide a summary of the news article included below and don't deviate from the format.\n\n#### Main thesis\n\nPut the main thesis of the news article here.\n\n#### Key facts\n\n* First fact in the article, with the source in parenthesis if the article cites another source\n* Second fact, again with the source if any\n* And so on\n\nBEGIN NEWS ARTICLE\n{article['article']}\nEND NEWS ARTICLE",
-            },
-            {"role": "assistant", "content": prefill},
-        ],
-    )
-    text = prefill
-    for chunk in stream:
-        text += chunk.choices[0].delta.content or ""
-        html = markdown.markdown(text)  # Convert the markdown to HTML
-        emit(
-            "token",
-            {
-                "id": article.id,
-                "text": html,
-            },
-        )
+    def add_metadata(self, date, url, publication):
+        date = int(str(date)[:-3])  # Chop off the last three digits
+        date = datetime.datetime.fromtimestamp(date, datetime.UTC)
+        self.date = date.strftime("%a, %B %d %Y")
+        self.url = url
+        self.publication = publication
